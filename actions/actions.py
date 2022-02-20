080a65d54f932f92064d76c88132c74b25327fb6
Steve Cao <steve@yojee.com>
2022-02-20 18:09:09 +0800

    add actions and update domain

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,50 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from actions.food_finder import FoodFinder
+
+
+class ActionShowFoods(Action):
+
+    def name(self) -> Text:
+        return "action_show_foods"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        _place = tracker.get_slot("place")
+        if not _place:
+            dispatcher.utter_message(text="Sorry I didn't get your place")
+            return [SlotSet("place", None)]
+        _found_place = FoodFinder.find_place_in_db(str(_place))
+        if not _found_place:
+            dispatcher.utter_message(text="Sorry, I have not idea about the place {}. You may try another place.")
+            return [SlotSet("place", None)]
+        _category = tracker.get_slot("food_category")
+        if not _category:
+            dispatcher.utter_message(text="Sorry I didn't get your food_category")
+            return [SlotSet("food_category", None)]
+        _found_category = FoodFinder.find_place_in_db(str(_place))
+        if not _found_category:
+            dispatcher.utter_message(text="Sorry, I have not idea about the food {}. You may try another kind of food.")
+            return [SlotSet("food_category", None)]
+        _rests = FoodFinder.find_restaurants(place=_found_place, food_category=_found_category)
+        if not _rests:
+            dispatcher.utter_message(text="Sorry, I have find {} food at {}".format(_category, _found_place))
+            dispatcher.utter_message(text="You can try other foods or at other places")
+            return [SlotSet("food_category", None), SlotSet("place", None)]
+        _attachments = {
+            "type": "template",
+            "payload": {
+                "template_type": "generic"
+            }
+        }
+        _elements = [{"title": r["title"], "image_url": r["imageUrl"], "buttons":[]} for r in _rests]
+        _attachments["payload"]["elements"] = _elements
+        dispatcher.utter_message(text="Here are the foods found for you:", attachment=str(_attachments))
+        return [SlotSet("food_category", None), SlotSet("place", None)]
